c773e1a7b26e7dfedb6f0d23d3fa499001075852
Sanskar Jethi <sansyrox@gmail.com>
2019-06-02 00:17:07 +0530

    Library Restructure

--- susi_python/__init__.py
+++ susi_python/__init__.py
@@ -1,9 +1,11 @@
-from .main import use_api_endpoint
-from .main import query
-from .main import ask
-from .main import answer_from_json
-from .main import sign_in
-from .main import sign_up
-from .main import forgot_password
-from .main import get_previous_responses
-from .main import update_location
+from .main import main
+
+use_api_endpoint = main.use_api_endpoint
+query = main.query
+ask = main.ask
+answer_from_json = main.answer_from_json
+sign_in = main.sign_in
+sign_up = main.sign_up
+forgot_password = main.forgot_password
+get_previous_responses = main.get_previous_responses
+update_location = main.update_location
