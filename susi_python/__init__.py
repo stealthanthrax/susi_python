70c94a4468deb3feee97b90fbf03d5bf72692384
Sanskar Jethi <sansyrox@gmail.com>
2019-06-13 17:15:11 +0530

    removed extra variables

--- susi_python/__init__.py
+++ susi_python/__init__.py
@@ -1,11 +1,1 @@
 from .main import susi_client
-
-use_api_endpoint = susi_client.use_base_url
-query = susi_client.query
-ask = susi_client.ask
-answer_from_json = susi_client.answer_from_json
-sign_in = susi_client.sign_in
-sign_up = susi_client.sign_up
-forgot_password = susi_client.forgot_password
-get_previous_responses = susi_client.get_previous_responses
-update_location = susi_client.update_location
